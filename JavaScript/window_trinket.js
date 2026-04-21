7a612aa823c385700211e09f7dd0485cb7c51184
viktorvulchanov <viktor.m.vulchanov3000@gmail.com>
2026-04-21 14:59:29 +0300

    Update window_trinket.js

--- JavaScript/window_trinket.js
+++ JavaScript/window_trinket.js
@@ -12,7 +12,7 @@ p_proz = window.open("", "", "top=250, left=620, width=600, height=730");
 p_proz.document.write("<html>");
 p_proz.document.write("<head><title>How much money will you save</title></head>");
 p_proz.document.write("<div>");
-p_proz.document.write("<iframe src=https://trinket.io/embed/python/5868181003fb?toggleCode=true&start=result width=100% height=100% frameborder=0 marginwidth=0 marginheight=0 allowfullscreen></iframe>");
+p_proz.document.write("<iframe src=https://trinket.io/embed/python/1fc50ce793a0?toggleCode=true&start=result width=100% height=100% frameborder=0 marginwidth=0 marginheight=0 allowfullscreen></iframe>");
 p_proz.document.write("</div>");
 p_proz.document.write("</html>");
 //HTML прозорец затваряне
